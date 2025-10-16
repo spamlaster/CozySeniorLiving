457a9e6f339f9563e3ecc0f19d773651448e95b3
Nick Lancaster <nicklan@gmail.com>
2025-10-16 17:18:23 -0600

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   plugins: [vue()],
-  base: 'cozyseniorliving.com',
+  base: '/',
   build: { outDir: 'docs', emptyOutDir: true }
 })
