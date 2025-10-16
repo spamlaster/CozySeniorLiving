ac4f7fc48e3bda55713a21103ce7f7dc38d648a2
Nick Lancaster <nicklan@gmail.com>
2025-10-16 17:17:34 -0600

    Publish Vite build to /docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: 'cozyseniorliving.com',
+  build: { outDir: 'docs', emptyOutDir: true }
 })
